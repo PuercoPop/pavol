392b7be09bc6311136fb24053df4b431f4b78a80
Diogo F. S. Ramos <diogofsr@gmail.com>
2012-04-24 05:16:41 -0300

    Use simpler names
    Now that the code is behind a package, there is no need to prefix
    functions with `pavol-'.

--- pavol.lisp
+++ pavol.lisp
@@ -6,7 +6,7 @@
 
 (defparameter *pavol-max* 65536)
 
-(defun pavol-volume ()
+(defun volume ()
   (let ((str-sinks
          (stumpwm:run-shell-command "pacmd list-sinks" t)))
     (multiple-value-bind (start end start-reg end-reg)
@@ -14,7 +14,7 @@
       (declare (ignore start end))
       (parse-integer (subseq str-sinks (elt start-reg 0) (elt end-reg 0))))))
 
-(defun pavol-mute-p ()
+(defun mutep ()
   (let ((str-sinks
          (stumpwm:run-shell-command "pacmd list-sinks" t)))
     (multiple-value-bind (start end start-reg end-reg)
@@ -22,56 +22,56 @@
       (declare (ignore start end))
       (string= "yes" (subseq str-sinks (elt start-reg 0) (elt end-reg 0))))))
 
-(defun pavol-percentage->integer (percentage)
+(defun percentage->integer (percentage)
   (let ((r (truncate (* *pavol-max* percentage) 100)))
     r))
 
-(defun pavol-set-volume (percentage)
+(defun set-volume (percentage)
   (stumpwm:run-shell-command
    (format nil "pacmd set-sink-volume 0 ~a"
-           (pavol-percentage->integer percentage))))
+           (percentage->integer percentage))))
 
-(defun pavol-mute (state)
+(defun mute (state)
   (stumpwm:run-shell-command
    (format nil "pacmd set-sink-mute 0 ~a" (if state 1 0))))
 
-(defun pavol-show-volume-bar ()
-  (let ((percent (pavol-volume)))
+(defun show-volume-bar ()
+  (let ((percent (volume)))
     (stumpwm:message
      (concatenate 'string
-      (format nil "~:[OPEN~;MUTED~]" (pavol-mute-p))
+      (format nil "~:[OPEN~;MUTED~]" (mutep))
       (format nil "~C^B~A%" #\Newline percent) "^b [^[^7*"
       (stumpwm::bar percent 50 #\# #\:) "^]]"))))
 
-(defun pavol-volume-up (percentage)
+(defun volume-up (percentage)
   (let ((new-percentage
-         (let ((cur (pavol-volume)))
+         (let ((cur (volume)))
            (if (> (+ cur percentage) 100)
                100
                (+ cur percentage)))))
-    (pavol-set-volume new-percentage)))
+    (set-volume new-percentage)))
 
-(defun pavol-volume-down (percentage)
+(defun volume-down (percentage)
   (let ((new-percentage
-         (let ((cur (pavol-volume)))
+         (let ((cur (volume)))
            (if (< (- cur percentage) 0)
                0
                (- cur percentage)))))
-    (pavol-set-volume new-percentage)))
+    (set-volume new-percentage)))
 
 (defun vol+ (percentage)
-  (pavol-volume-up percentage)
-  (pavol-show-volume-bar))
+  (volume-up percentage)
+  (show-volume-bar))
 
 (defun vol- (percentage)
-  (pavol-volume-down percentage)
-  (pavol-show-volume-bar))
+  (volume-down percentage)
+  (show-volume-bar))
 
 (defun toggle-mute ()
-  (if (pavol-mute-p)
-      (pavol-mute nil)
-      (pavol-mute t))
-  (pavol-show-volume-bar))
+  (if (mutep)
+      (mute nil)
+      (mute t))
+  (show-volume-bar))
 
 (in-package #:stumpwm)
 
